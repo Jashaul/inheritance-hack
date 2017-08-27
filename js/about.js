b770b9ccc3647a6d3f186fe4b047f7bf0588a76a
Adarsh M <adarshm97@outlook.com>
2017-08-27 09:51:47 +0530

    almost doneeeeee

--- js/about.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function(){
-	$('#nav-icon').click(function(){
-		$(this).toggleClass('open');
-		if($('#mySidenav').css('width') === '400px')
-			$('#mySidenav').css('width', '0px');
-		else 
-			$('#mySidenav').css('width', '400px');
-	});
-});
