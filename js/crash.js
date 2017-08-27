c34c21b3f8f6dd79ee6c82636d84174adbf908b8
Adarsh M <adarshm97@outlook.com>
2017-08-27 06:59:09 +0530

    crash base

---
+++ js/crash.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+	$('#nav-icon').click(function(){
+		$(this).toggleClass('open');
+		if($('#mySidenav').css('width') === '400px')
+			$('#mySidenav').css('width', '0px');
+		else 
+			$('#mySidenav').css('width', '400px');
+	});
+});
